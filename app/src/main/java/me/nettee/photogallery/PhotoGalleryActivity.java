0c7e10f9780b0b6f0b8a69bdb2c32c9e2aec8aae
William <nettee.liu@gmail.com>
2017-07-20 19:52:44 +0800

    add drawer navigation

--- app/src/main/java/me/nettee/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/me/nettee/photogallery/PhotoGalleryActivity.java
@@ -1,14 +1,53 @@
 package me.nettee.photogallery;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class PhotoGalleryActivity extends SingleFragmentActivity {
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
-    protected Fragment createFragement() {
-        return PhotoGalleryFragment.newInstance();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+
+        mToolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+
+        // Set the fragment
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.photo_gallery_fragment);
+
+        if (fragment == null) {
+            fragment = new PhotoGalleryFragment();
+            fm.beginTransaction()
+                    .add(R.id.photo_gallery_fragment, fragment)
+                    .commit();
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the drawer when the icon is clicked.
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
